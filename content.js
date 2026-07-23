59afa0f17e11cd362f5a191dce64cdcc41285325
Avika Negi <AVIKA0315@GMAIL.COM>
2026-07-23 20:13:06 +0530

    Initial LeetCoach extension

---
+++ content.js
@@ -0,0 +1,20 @@
+const titleElement = document.querySelector('a[href^="/problems/"]');
+const difficultyElement = document.querySelector('[class*="difficulty-"]');
+
+const paragraphs = document.querySelectorAll("p");
+
+let description = "";
+
+for (let i = 0; i < 3; i++) {
+    description += `${paragraphs[i].textContent.trim()}\n\n`;
+}
+
+const problem = {
+    title: titleElement.textContent.trim(),
+    difficulty: difficultyElement.textContent.trim(),
+    description: description
+};
+
+console.log("Title:", problem.title);
+console.log("Difficulty:", problem.difficulty);
+console.log("Description:\n" + problem.description);
